33b93b331c9066caf3ca9f2c62e6d41a7deca326
Krishna Prasad A <kpa.krishnaprasada@gmail.com>
2025-10-20 22:42:00 +0530

    feature: Project sturcture added

---
+++ src/test/java/dev/krishnaprasad/simple_todo/SimpleTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.krishnaprasad.simple_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
